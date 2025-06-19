1d0db56e365d4481e80589d9902e3b7b02776091
Andras Solyom <andras.solyom@genesys.com>
2025-06-19 16:24:41 +0200

    [Update] 1.14.1

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GenesysCloudCore",
-            url: "https://genesysdx.jfrog.io/artifactory/genesys-cloud-ios.prod/GenesysCloudCore/GenesysCloudCore_version_t1.14.0_commit_eae528ea44da6a2cd11d1414b6cb74cedd8d9451.zip",
-            checksum: "055003d82799734a20e98936051b7ed45d7762bf6ae313e78f5a45d5564e9315"
+            url: "https://genesysdx.jfrog.io/artifactory/genesys-cloud-ios.prod/GenesysCloudCore/GenesysCloudCore_version_t1.14.1_commit_eae528ea44da6a2cd11d1414b6cb74cedd8d9451.zip",
+            checksum: "97186cb4afb08d910192d7f41ead7216c67b64ba0ba817ec185c9c581f0507d8"
         ),
         .testTarget(name: "PackageTests", dependencies: [
             "GenesysCloudCore"
