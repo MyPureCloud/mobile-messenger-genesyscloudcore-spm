7610a89fe9dddf5c16e80c80c615240def6b7fc8
PureCloud Jenkins <purecloud-jenkins@ininica.com>
2026-06-11 18:26:13 +0000

    [Fix] 2.0.1

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GenesysCloudCore",
-            url: "https://genesysdx.jfrog.io/artifactory/genesys-cloud-ios.prod/GenesysCloudCore/GenesysCloudCore_version_2.0.0-rc8_commit_ce35f764c40611365fbb121fa5f9a713165df610.zip",
+            url: "https://github.com/MyPureCloud/mobile-messenger-ios-sdk/releases/download/v2.0.0/GenesysCloudCore.zip",
             checksum: "78e1b386ae4c975d528b810085894ab6a6328b4e9ce4bd423f59ea0aed61cf43"
         ),
         .testTarget(name: "PackageTests", dependencies: [
